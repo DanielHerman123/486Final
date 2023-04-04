49a1db64805d73b666b63143e3f1f7daef474340
danherm <danherm@umich.edu>
2023-04-04 19:30:40 -0400

    removed initial code and have reddit api config

--- main.py
+++ main.py
@@ -1,32 +1,12 @@
 import requests
 import argparse
 import os
+import praw
 
-# Parse command line arguments
-parser = argparse.ArgumentParser(description='Scrape Reddit post text from specified communities and store them in a directory within the current directory')
-parser.add_argument('communities', nargs='+', help='List of communities to scrape')
-args = parser.parse_args()
+reddit = praw.Reddit(
+    client_id='your_client_id',
+    client_secret='your_client_secret',
+    redirect_uri='http://localhost:8000',
+    user_agent='your_user_agent'
+)
 
-# Set up Reddit API request headers and parameters
-headers = {'User-Agent': 'MyBot/0.0.1'}
-params = {'sort': 'top', 't': 'week', 'limit': 10}
-
-# Create directory to store scraped text files
-dir_name = 'reddit_scraped_text'
-if not os.path.exists(dir_name):
-    os.makedirs(dir_name)
-
-# Loop through specified communities
-for community in args.communities:
-    # Send Reddit API request to get top posts from past week
-    url = f'https://www.reddit.com/r/{community}/top.json'
-    response = requests.get(url, headers=headers, params=params)
-
-    # Parse JSON response and loop through top posts to save text to file
-    data = response.json()
-    for post in data['data']['children']:
-        text = post['data']['selftext']
-        if text:
-            filename = f"{dir_name}/{community}_{post['data']['id']}.txt"
-            with open(filename, 'w') as f:
-                f.write(text)
