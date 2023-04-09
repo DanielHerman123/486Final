7a8533204e1bc664be0be906c45f8efe13e1b1cb
danherm <danherm@umich.edu>
2023-04-09 16:41:17 -0400

    changed training data from .txt files to a single .csv file

--- trainScraper.py
+++ trainScraper.py
@@ -3,6 +3,7 @@ import os
 import sys
 import praw
 import shutil
+import csv
 
 reddit = praw.Reddit(
     client_id='6wqQI91lq4Vp2YTxGl5JFw',
@@ -13,51 +14,37 @@ reddit = praw.Reddit(
 
 def collect(amountTrain):
     """collect reddit posts"""
-    # f = open('/trainingData', 'w')
 
     # make call to api with provided communities
     communities_left = ['Democrats']    # should include more
     communities_right = ['Republican']
 
-    # Loop through Left communities and download to trainingData directory
-    count = 0
-    for Lcommunity in communities_left:
-        subreddit = reddit.subreddit(Lcommunity)
-        for post in subreddit.new(limit=amountTrain):
-            # Check if post is a text post
-            # if post.is_self:
-            # Create filename based on post title
-            filename = f"trainingData/left{count}.txt"
-            # Write post text to file
-            with open(filename, 'w') as f:
-                f.write(post.title)
-                if hasattr(post, 'selftext') and post.selftext != "":
-                    print("left", count)
-                    f.write('\n')
-                    f.write(post.selftext)
-                count += 1
+    # Prepare a CSV file for the combined data
+    with open('trainingData/training_data.csv', mode='w', newline='', encoding='utf-8') as combined_file:
+        combined_writer = csv.writer(combined_file)
 
-    # Loop through Right communities and download to trainingData directory
-    count = 0
-    for Rcommunity in communities_right:
-        subreddit = reddit.subreddit(Rcommunity)
-        # print(subreddit)
-        for post in subreddit.new(limit=amountTrain):
-            # Check if post is a text post
-            # if post.is_self:
-            # print(post.selftext)
-            # Create filename based on post title
-            filename = f"trainingData/right{count}.txt"
-            # Write post text to file
-            with open(filename, 'w') as f:
-                f.write(post.title)
-                if hasattr(post, 'selftext') and post.selftext != "":
-                    print("right", count)
-                    f.write('\n')
-                    f.write(post.selftext)
-                count += 1
+        # Write header row for the CSV file
+        combined_writer.writerow(['text', 'label'])
 
-    # f.close()
+        # Loop through Left communities and download to trainingData directory
+        for Lcommunity in communities_left:
+            subreddit = reddit.subreddit(Lcommunity)
+            for post in subreddit.new(limit=amountTrain):
+                text = post.title
+                if hasattr(post, 'selftext') and post.selftext != "":
+                    text += '\n' + post.selftext
+                # Write row to CSV file
+                combined_writer.writerow([text, 'left'])
+
+        # Loop through Right communities and download to trainingData directory
+        for Rcommunity in communities_right:
+            subreddit = reddit.subreddit(Rcommunity)
+            for post in subreddit.new(limit=amountTrain):
+                text = post.title
+                if hasattr(post, 'selftext') and post.selftext != "":
+                    text += '\n' + post.selftext
+                # Write row to CSV file
+                combined_writer.writerow([text, 'right'])
 
 def main(amountTrain):
     """Main Function."""
